12c94e0dafec5533867040a1bd19eb0f8c55e88a
jnaky4 <jnaky4@users.noreply.github.com>
2020-04-22 18:52:15 -0500

    about to merge to projects

--- Cat - A - Pult/Assets/Bounce.cs
+++ Cat - A - Pult/Assets/Bounce.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Rigidbody rb;
+
+
+
+
+    public float radius = 0.0f;
+    public float power = 2000.0f;
+    public float upwardsModifier = 0.0f;
+
+    Vector3 epicentro;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Vector3 explosionPos = transform.position;
+        epicentro = explosionPos;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 }

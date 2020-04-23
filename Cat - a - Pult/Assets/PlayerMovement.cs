fd5001f50b6d98c02606c20e0006048c282f1c24
jnaky4 <jnaky4@users.noreply.github.com>
2020-04-22 21:54:33 -0500

    updates

--- Cat - a - Pult/Assets/PlayerMovement.cs
+++ Cat - a - Pult/Assets/PlayerMovement.cs
@@ -17,14 +17,29 @@ public class PlayerMovement : MonoBehaviour
     // FixedUpdate is used for physics and performs better
     void FixedUpdate()
     {
-        rb.AddForce(0, verticalForce * Time.deltaTime, 0);
+        //rb.AddForce(0, verticalForce * Time.deltaTime, 0);
+        
         if (Input.GetKey("d"))
         {
-            rb.AddForce(500 * Time.deltaTime, 0 , 0);
+            rb.AddForce(300 * Time.deltaTime, 0 , 0, ForceMode.VelocityChange);
         }
+
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+            rb.AddForce(-300 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0,300 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -300 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, 500 * Time.deltaTime, 0, ForceMode.VelocityChange);
         }
     }
 }

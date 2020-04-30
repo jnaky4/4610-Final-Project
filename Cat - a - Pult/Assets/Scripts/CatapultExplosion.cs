ff4c8c194d6f5e9af2faa3f211a4c34b7273ece2
jnaky4 <jnaky4@users.noreply.github.com>
2020-04-29 20:15:57 -0500

    Update

---
+++ Cat - a - Pult/Assets/Scripts/CatapultExplosion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultExplosion : MonoBehaviour
+{
+    public float radius;
+    public float explosionSpeed;
+    public float power;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Catapult")
+        {
+            explode();
+        }
+    }
+    public void explode()
+    {
+
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0f);
+
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
